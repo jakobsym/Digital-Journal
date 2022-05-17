043db672db1f6ab19b3437247da729a56378e9f4
Jakob <jakob.symchych@my.utsa.edu>
2022-05-17 16:16:52 -0500

    activating models

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+# Class allowing user to create topic 
+class Topic(models.Model):
+    """ A topic the user is learning about """
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self):
+        """ Returning a string """
+        return self.text
