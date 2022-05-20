da9d6e7db0de32614e20e0bb8ebb416a7acf64d6
Jakob <jakob.symchych@my.utsa.edu>
2022-05-20 17:56:49 -0500

    added topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Topic
+
 
 def index(request):
     """ The homepage for learning_logs """
     return render(request, 'learning_logs/index.html')
-# Create your views here.
+
+def topics(request):
+    """ Show all topics """
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+    
