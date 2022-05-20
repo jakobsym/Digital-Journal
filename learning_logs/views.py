fb73f1c59e59ae7d5aa18ff35c20cb4224d30613
Jakob <jakob.symchych@my.utsa.edu>
2022-05-20 15:58:56 -0500

    adding view for template

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+def index(request):
+    """ The homepage for learning_logs """
+    return render(request, 'learning_logs/index.html')
 # Create your views here.
