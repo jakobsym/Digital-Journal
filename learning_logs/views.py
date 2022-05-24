d721df8f60e76fbe80b54d720f09d75b033ab27e
Jakob <jakob.symchych@my.utsa.edu>
2022-05-24 12:52:44 -0500

    adding function

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,8 @@
+from pickletools import OpcodeInfo
 from django.shortcuts import render, redirect
 
 from .models import Topic
-from .forms import TopicForm  #'TopicForm' is a class from 'forms.py'
+from .forms import TopicForm, EntryForm  #'TopicForm' is a class from 'forms.py'
 
 
 
@@ -36,3 +37,21 @@ def new_topic(request):
     # display blank/invalid form
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """ allow for new entry of a topic """
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # No data
+        form = EntryForm()
+    else:
+        # Process data
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
