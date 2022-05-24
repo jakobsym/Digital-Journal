90148ef58b013f94919795a6f70c9718c66eb39f
Jakob <jakob.symchych@my.utsa.edu>
2022-05-24 15:09:33 -0500

    adding function

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from pickletools import OpcodeInfo
 from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm  #'TopicForm' is a class from 'forms.py'
 
 
@@ -55,3 +55,21 @@ def new_entry(request, topic_id):
             return redirect('learning_logs:topic', topic_id=topic_id)
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """ Edit exisiting entry """
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic:': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+ 
+
