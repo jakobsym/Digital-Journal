421f369f4d2cc0aaa0c53cca2594f537397e0f7c
Jakob <jakob.symchych@my.utsa.edu>
2022-05-24 15:25:13 -0500

    fixing typo

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -16,5 +16,5 @@ urlpatterns = [
     # Page for adding new entries
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
     # Page for editing entries
-    path('edit_entry/<int:topic_id>/', views.edit_entry, name='edit_entry'),
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
